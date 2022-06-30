f7ff29340d8a4232465c76deb7a3b7b07c520f5f
Quianna Rolston <106702744+qrolston@users.noreply.github.com>
2022-06-29 20:29:44 -0700

    Initial commit for app.py utilities

---
+++ qualifier/utils/fileio.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""Helper functions to load and save CSV data.
+
+This contains a helper function for loading and saving CSV files.
+
+"""
+import csv
+
+
+def load_csv(csvpath):
+    """Reads the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+
+    Returns:
+        A list of lists that contains the rows of data from the CSV file.
+
+    """
+    return new_func(csvpath)
+
+def new_func(csvpath):
+    with open(csvpath, "r") as csvfile:
+        data = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Skip the CSV Header
+        next(csvreader)
+
+        # Read the CSV data
+        for row in csvreader:
+            data.append(row)
+    return data
+
+def save_csv(csv_loc, qualifying_loans, header):
+    """Saves the CSV file from path provided.
+
+    Args:
+        csv_loc (Path): The CSV file path/location
+        data (list of lists): A list of the rows of data for the CSV file.
+        header (list): An optional header for the CSV.
+
+    """
+    with open(csv_loc, "w", newline="") as csvfile:
+    
+        # Create a csvwriter
+        csvwriter = csv.writer(csvfile, delimiter=",")
+
+        # Write the header to the CSV file
+        csvwriter.writerow(header)
+
+         # Write the values of each dictionary 
+         # as a row in the CSV file.
+        for row in qualifying_loans:
+            csvwriter.writerow(row)
